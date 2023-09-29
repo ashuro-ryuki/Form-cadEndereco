fed6e555f23c01bcddb0f45f9ae0b987daadeff9
Leonardo Fernando <lffxds@gmai.com>
2023-09-29 11:49:04 -0300

    add função preencher formulario + cep valido

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ## descrição
 Este código HTML cria um formulário de cadastro de endereço usando a biblioteca Bootstrap para estilos. O formulário inclui campos para inserir o CEP, endereço, número, bairro, cidade e estado. Um botão "Cadastrar" está presente para enviar o formulário.
+//add imagem da fonte bootrap5
+https://getbootstrap.com/docs/5.0/layout/gutters/
--- controller.js
+++ controller.js
@@ -5,8 +5,18 @@
 
 // Função para limpar o formulário
 const limparFormulario = () => {
-   document.getElementById('rua').value
-   document.getElementById('bairro').value
-   document.getElementById('estado').value
-   document.getElementById('cidade').value
+   document.getElementById('rua').value = '';
+   document.getElementById('bairro').value = '';
+   document.getElementById('estado').value = '';
+   document.getElementById('cidade').value = '';
+}
+// verifica se CEP é valido
+const eNumero = (numero) => /^[0-9]+$/.test(numero);// testa numero infromado com expressão regular
+ 
+//função para preencher fomrulario
+const  preencherFormulario = (endereco) => {
+   document.getElementById('rua').value = endereco.logadouro
+   document.getElementById('bairro').value = endereco.bairro
+   document.getElementById('estado').value = endereco.uf
+   document.getElementById('cidade').value = endereco.localidade
 }
